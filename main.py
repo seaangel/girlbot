f8a55634cd6080038b60e58b5fb67ace5e532717
Roma <roma01@yandex.ru>
2021-06-25 15:26:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,17 +22,52 @@ def cont(message):
     DIR = 'girl'
     with open ('text/text.txt', 'r',encoding="utf-8") as file:
         lines = file.readlines()
-        life = random.choice(lines)
-    boobs = str(random.randint(85,95))+'🍒'+' '+str(random.randint(60,70))+'⌛️'+' '+str(random.randint(80,100))+'🍑'
+        name = 'Имя: ' + random.choice(lines)
+    boobs = '\nРазмеры: '+str(random.randint(85,95))+'🍒'+' '+str(random.randint(60,70))+'⌛️'+' '+str(random.randint(80,100))+'🍑'
+    age = 'Возраст: '+ str(random.randint(18,30))
     adr = 'www.ya.ru'
-    all_life = life +boobs +'\n'+'\n'+adr
+    
+    
+    
+    markup1 = telebot.types.InlineKeyboardMarkup()
+    btn = [telebot.types.InlineKeyboardButton(text='🔥', url=adr),telebot.types.InlineKeyboardButton(text='➡️', callback_data='buy2')]
+    
+    markup1.add(*btn)
+    
+    
+    all_life = name + age + boobs +'\n'+'\n'+adr
     if message.text == 'Покажи девушку':
         
-       # bot.send_photo(message.chat.id, open('girl/52LBCrywU28.jpg', 'rb'),caption='Ирина - 30 лет \n www.ya.ru')
-        bot.send_photo(message.chat.id, open(os.path.join(DIR, random.choice(os.listdir(DIR))),'rb'),caption=all_life)
-        #bot.send_message(message.chat.id, 'Ирина - 30 лет \n www.ya.ru', reply_markup=main())
+       
+        bot.send_photo(message.chat.id, open(os.path.join(DIR, random.choice(os.listdir(DIR))),'rb'),caption=all_life,reply_markup=markup1)
+        
 
     else:
         bot.send_message(message.chat.id, 'Я тебя не понимаю', reply_markup=main())
-
+        
+#
+      
+@bot.callback_query_handler(func=lambda message:True)
+def ans(message):
+    cid = message.message.chat.id
+    keyboard = types.InlineKeyboardMarkup()
+    DIR = 'girl'
+    with open ('text/text.txt', 'r',encoding="utf-8") as file:
+        lines = file.readlines()
+        name = 'Имя: ' + random.choice(lines)
+    boobs = '\nРазмеры: '+str(random.randint(85,95))+'🍒'+' '+str(random.randint(60,70))+'⌛️'+' '+str(random.randint(80,100))+'🍑'
+    age = 'Возраст: '+ str(random.randint(18,30))
+    adr = 'www.ya.ru'
+    all_life = name + age + boobs +'\n'+'\n'+adr
+    markup1 = telebot.types.InlineKeyboardMarkup()
+    btn = [telebot.types.InlineKeyboardButton(text='🔥', url=adr),telebot.types.InlineKeyboardButton(text='➡️', callback_data='buy2')]
+    
+    markup1.add(*btn)
+    if message.data == "buy2":
+        bot.send_message(cid, "Нажали 1-ю кнопку",reply_markup=keyboard)
+        bot.send_photo(cid, open(os.path.join(DIR, random.choice(os.listdir(DIR))),'rb'),caption=all_life,reply_markup=markup1)
+      
+       
+   
+    
 bot.polling()
